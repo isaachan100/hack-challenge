b7f64656bb57267c8ae9e227fdf994d3651fc4ce
DashingDolphin <andrewcheung360@gmail.com>
2022-11-23 16:52:30 -0500

    initial xcode project

---
+++ BigRedBounty/BigRedBounty/BigRedBountyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigRedBountyApp.swift
+//  BigRedBounty
+//
+//  Created by user228377 on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BigRedBountyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
