2f2a2c055764b9ff12c160ff271f558a83add0f5
Isaac Han <isaac.han100@gmail.com>
2022-11-29 16:37:30 -0500

    added routes and specs to app.py

--- app.py
+++ app.py
@@ -28,12 +28,17 @@ def failure_response(message, code = 400):
 
 @app.route("/api/users/", methods=["GET"])
 def get_users():
+    """
+    returns all users in the database
+    """
     users = [User.serialize() for User in User.query.all()]
     return success_response({'Users': users})
 
-
 @app.route("/api/users/", methods=["POST"])
 def create_user():
+    """
+    creates a new user in the database
+    """
     body = json.loads(request.data)
     name = body.get("name")
     email = body.get("email")
@@ -54,6 +59,9 @@ def create_user():
 
 @app.route("/api/users/<int:user_id>/", methods=["GET"])
 def get_user(user_id):
+    """
+    returns a specific user based on their user id
+    """
     user = User.query.filter_by(id = user_id).first()
     if not user:
         return failure_response("User not found", 404)
@@ -62,6 +70,9 @@ def get_user(user_id):
 
 @app.route("/api/users/<int:user_id>/", methods=["DELETE"])
 def delete_user(user_id):
+    """
+    deletes a user 
+    """
     user = User.query.filter_by(id = user_id).first()
     if not user:
         return failure_response("User not found", 404)
@@ -70,26 +81,62 @@ def delete_user(user_id):
     return success_response(user.serialize())
 
 # auth route here
+@app.route("/api/users/login/", methods = ["POST"])
+def login():
+    """
+    attempts to verify user and returns session token if successful
+    """
+    pass
 
 # Item routes
 
 @app.route("/api/items/", methods=["GET"])
 def get_items():
+    """
+    returns all items
+    """
     items = [Item.serialize() for Item in Item.query.all()]
     return success_response({'Items': items})
 
 @app.route("/api/items/", methods=["POST"])
 def create_item():
+    """
+    creates an item
+    """
     pass
 
 @app.route("/api/items/<int:item_id>/", methods=["GET"])
 def get_item(item_id):
+    """
+    returns a specific item based on item id
+    """
     pass
 
 @app.route("/api/items/<int:item_id>/", methods=["DELETE"])
 def delete_item(item_id):
+    """
+    deletes an item based on item id
+    """
     pass
 
+@app.route("/api/items/<int:item_id>/", methods = ["POST"])
+def update_item(item_id):
+    """
+    updates an item given item id
+    """
+    pass
+
+@app.route("/api/items/claim/<int:item_id/", methods = ["POST"])
+def submit_claim(item_id):
+    """
+    allows a user to submit a claim for an item
+    """
+    pass
+
+#create locations
+"""
+creates location objects and adds them to the database
+"""
 
 
 if __name__ == "__main__":
